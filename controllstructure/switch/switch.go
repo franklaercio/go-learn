d0ce578fa3a00a2b5b92e8b2caa68b68123679c4
Frank Laercio <franklaercio@gmail.com>
2022-12-04 23:13:44 -0300

    feat: adding switch structure

--- controllstructure/switch/switch.go
+++ controllstructure/switch/switch.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func grade(g float64) string {
 	var grade = int(g)
@@ -29,4 +32,15 @@ func main() {
 	fmt.Println(grade(5))
 	fmt.Println(grade(2))
 	fmt.Println(grade(-1))
+
+	t := time.Now()
+
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("Good Morning!")
+	case t.Hour() < 18:
+		fmt.Println("Good afternoon!")
+	default:
+		fmt.Println("Good Night!")
+	}
 }
